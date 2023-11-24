9c5f66fb681e53edff74856c0ad0f80436f1b3fd
LKosters <main@laurenskosters.nl>
2023-11-24 17:11:05 +0100

    added file structure for scrapers

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 import json
 from flask import render_template
 
+from scrapers.melee_weapons import scrape_melee_weapons
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,41 +14,8 @@ def index():
     return render_template('index.html')
     
 @app.route('/api/v1/weapons', methods=['GET'])
-def scrape_and_return_json():
-    
-    url = 'https://deadcells.wiki.gg/wiki/Melee_weapons'
-    response = requests.get(url)
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    table = soup.find('table', attrs={'class': 'wikitable'})
-    table_body = table.find('tbody')
-    rows = table_body.find_all('tr')
-
-    data = []
-    for row in rows[1:]:  
-
-        cols = row.find_all('td')
-        cols = [ele.text.strip() for ele in cols]
-
-        images = row.find_all('img')  # Get all images in the row
-        
-        weapon_img = 'https://deadcells.wiki.gg' + images[0]['src']  # Get the source of the first image 
-
-        scaling_img = 'https://deadcells.wiki.gg' + images[1]['src']  # Get the source of the second image
-        
-
-        weapon_data = {
-            'weapon': cols[1],
-            'description': cols[2],
-            'blueprint_location': cols[3],
-            'base_dps': cols[4],
-            'scaling': scaling_img, 
-            'weapon_img': weapon_img
-        }
-        data.append(weapon_data)
-
-    return jsonify(data)
+def get_weapons():
+    return scrape_melee_weapons()
 
 if __name__ == '__main__':
     app.run(port=5000)
